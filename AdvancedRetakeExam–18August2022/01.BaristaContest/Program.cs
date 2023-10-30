e46c15d5951beb3af8deff507b1068961eba9d5a
sisssona <91665550+sisssona@users.noreply.github.com>
2023-10-30 17:43:08 +0200

    add

---
+++ AdvancedRetakeExam–18August2022/01.BaristaContest/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _01.BaristaContest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> coffee = new(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> milk = new(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int sum = 0;
+            Dictionary<string, int> coffeeProduct = new Dictionary<string, int>();
+            Dictionary<string, int> coffeeList = new Dictionary<string, int>()
+            {
+                {"Cortado", 50 },
+                {"Espresso", 75 },
+                {"Capuccino", 100 },
+                {"Americano", 150 },
+                {"Latte", 200 },
+            };
+            bool isMade = false;
+
+            while (coffee.Any() && milk.Any())
+            {
+                isMade = false;
+                sum = coffee.Peek() + milk.Peek();
+
+                foreach (var coffeeDrink in coffeeList)
+                {
+                    if (sum == coffeeDrink.Value)
+                    {
+                        if (!coffeeProduct.ContainsKey(coffeeDrink.Key))
+                        {
+                            coffeeProduct.Add(coffeeDrink.Key, 1);
+                        }
+                        else
+                        {
+                            coffeeProduct[coffeeDrink.Key]++;
+                        }
+                        coffee.Dequeue();
+                        milk.Pop();
+                        isMade = true;
+                        break;
+                    }
+
+                }
+
+                if (!isMade)
+                {
+                    if (coffee.Any())
+                    {
+                        coffee.Dequeue();
+                    }
+                    if (milk.Any())
+                    {
+                        int newMilk = milk.Pop() - 5;
+                        milk.Push(newMilk);
+                    }
+                }
+            }
+            //first line
+            var firstLine = coffee.Count == 0 && milk.Count == 0
+                ? "Nina is going to win! She used all the coffee and milk!"
+                : "Nina needs to exercise more! She didn't use all the coffee and milk!";
+            Console.WriteLine(firstLine);
+            //second line
+            var coffeeLeft = coffee.Count == 0 ? "none" : String.Join(", ", coffee);
+            Console.WriteLine($"Coffee left: {coffeeLeft}");
+            //third line
+            var milkLeft = milk.Count == 0 ? "none" : String.Join(", ", milk);
+            Console.WriteLine($"Milk left: {milkLeft}");
+
+            //fourth line
+            foreach (var drink in coffeeProduct.OrderBy(x => x.Value).ThenByDescending(x => x.Key))
+            {
+                Console.WriteLine($"{drink.Key}: {drink.Value}");
+            }
+        }
+    }
+}
---
+++ AdvancedRetakeExam–18August2022/02.Help-A-Mole/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _02.Help_A_Mole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[,] matrix = new string[n, n];
+            int startRow = -1;
+            int startCol = -1;
+            int points = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                string newRows = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    if (newRows[col].ToString() == "M")
+                    {
+                        startRow = row;
+                        startCol = col;
+                        matrix[row, col] = "-";
+                        continue;
+                    }
+                    matrix[row, col] = newRows[col].ToString();
+                }
+            }
+
+            string command;
+            while (true)
+            {
+                command = Console.ReadLine();
+                if (command == "End" || points >= 25)
+                {
+                    break;
+                }
+                if (OutofTheField(command, startRow, startCol, n))
+                {
+                    Console.WriteLine("Don't try to escape the playing field!");
+                    continue;
+                }
+                else
+                {
+                    if (command == "up")
+                    {
+                        startRow--;
+                    }
+                    else if (command == "down")
+                    {
+                        startRow++;
+                    }
+                    else if (command == "left")
+                    {
+                        startCol--;
+                    }
+                    else if (command == "right")
+                    {
+                        startCol++;
+                    }
+                }
+                if (Char.IsDigit(matrix[startRow, startCol][0]))
+                {
+                    points += int.Parse(matrix[startRow, startCol]);
+                    matrix[startRow, startCol] = "-";
+                    continue;
+
+                }
+                if (matrix[startRow, startCol] == "S")
+                {
+                    matrix[startRow, startCol] = "-";
+                    points -= 3;
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                    {
+                        for (int col = 0; col < matrix.GetLength(1); col++)
+                        {
+                            if (matrix[row,col] == "S")
+                            {
+                                startRow = row;
+                                startCol = col;
+                                matrix[startRow,startCol] = "-";    
+                            }
+                        }
+                    }
+                }
+
+            }
+            matrix[startRow, startCol] = "M";
+            if (points >= 25)
+            {
+                Console.WriteLine("Yay! The Mole survived another game!");
+                Console.WriteLine($"The Mole managed to survive with a total of {points} points.");
+            }
+            else
+            {
+                Console.WriteLine("Too bad! The Mole lost this battle!");
+                Console.WriteLine($"The Mole lost the game with a total of {points} points.");
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        private static bool OutofTheField(string command, int startRow, int startCol, int n)
+        {
+            if (command == "up" && startRow == 0 || command == "down" && startRow == n - 1 ||
+                command == "left" && startCol == 0 || command == "right" && startCol == n - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
